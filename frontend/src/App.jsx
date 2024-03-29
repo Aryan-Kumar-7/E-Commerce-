ef82e0f13d23f635147a256caae7a17bec421238
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-03-29 11:31:58 +0530

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css'
-import Navbar from './Compnents/NavBar/Navbar'
+import './App.css';
+import Navbar from './Compnents/NavBar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory category="men"/>}/>
+          <Route path='/womens' element={<ShopCategory category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path='product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>} />
+          </Route>
+          <Route path=':productId' element={<Cart/>} />
+          <Route path=':productId' element={<LoginSignup/>} />
+
+        </Routes>
+
+      </ BrowserRouter>
     </>
   )
 }
