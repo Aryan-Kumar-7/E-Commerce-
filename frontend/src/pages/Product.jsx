2ed6ca066b659dbcf6c5fac825bb4e18bf145a9f
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-04-04 19:22:53 +0530

    frontend

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Breadcrum from '../Compnents/Breadcrums/Breadcrum';
+import { useParams } from 'react-router-dom';
+import ProductDisplay from '../Compnents/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Compnents/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Compnents/RelatedProducts/RelatedProducts';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId))
   return (
     <div>
-      
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   )
 }
